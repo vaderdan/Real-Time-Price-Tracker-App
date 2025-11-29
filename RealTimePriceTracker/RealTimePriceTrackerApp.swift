6825eb47b3995ed5e48f1d415e19b71557d99bf6
Danny <danny.lazarow@gmail.com>
2025-11-29 07:21:44 +0200

    Initial Commit

---
+++ RealTimePriceTracker/RealTimePriceTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  RealTimePriceTrackerApp.swift
+//  RealTimePriceTracker
+//
+//  Created by danny on 29.11.25.
+//
+
+import SwiftUI
+
+@main
+struct RealTimePriceTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
